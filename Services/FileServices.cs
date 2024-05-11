8f7dbd14eed0a36807cd3545b96f828ffc99f922
ptinosq <tinosps@gmail.com>
2024-05-12 00:33:16 +0100

    Major refactoring

---
+++ Services/FileServices.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+
+namespace ElectroImageViewer.Services
+{
+    public class FileService
+    {
+        public static BitmapImage? OpenFile(string filePath)
+        {
+            try
+            {
+                BitmapImage bi = new();
+                using (FileStream stream = File.OpenRead(filePath))
+                {
+                    bi.BeginInit();
+                    bi.CacheOption = BitmapCacheOption.OnLoad; // Load the image with a FileStream to avoid file locking
+                    bi.StreamSource = stream;
+                    bi.EndInit();
+                }
+                bi.Freeze(); // Make the BitmapImage thread-safe
+                return bi;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error opening file: " + ex.Message);
+                return null;
+            }
+        }
+
+        public static string? OpenFileDialog()
+        {
+            OpenFileDialog openFileDialog = new();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                return openFileDialog.FileName;
+            }
+            return null;
+        }
+
+        public static bool RenameFile(string originalPath, string newName)
+        {
+            try
+            {
+                string directory = Path.GetDirectoryName(originalPath) ?? "./";
+                string newPath = Path.Combine(directory, newName);
+                File.Move(originalPath, newPath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to rename file: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static bool CopyFile(string sourcePath, string destination)
+        {
+            try
+            {
+                string fullPath = Path.GetFullPath(destination);
+                File.Copy(sourcePath, fullPath, overwrite: true);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Failed to copy file: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ Views/TerminalControl.xaml.cs
@@ -0,0 +1,121 @@
+﻿using ElectroImageViewer.Services;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace ElectroImageViewer
+{
+    /// <summary>
+    /// Interaction logic for TerminalControl.xaml
+    /// </summary>
+    /// 
+
+    public partial class TerminalControl : UserControl
+    {
+        public static readonly DependencyProperty? ControlVisibilityProperty = DependencyProperty.Register(
+        "ControlVisibility",
+        typeof(Visibility),
+        typeof(TerminalControl),
+        new PropertyMetadata(Visibility.Visible));
+
+        public Visibility ControlVisibility
+        {
+            get { return (Visibility)GetValue(ControlVisibilityProperty); }
+            set { SetValue(ControlVisibilityProperty, value); }
+        }
+
+        public TerminalControl()
+        {
+            InitializeComponent();
+        }
+
+        private void TerminalInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (DataContext is MainViewModel viewModel)
+            {
+                if (e.Key == Key.Enter)
+                {
+                    string command = terminalInput.Text;
+                    terminalOutput.Text += "> " + command + "\n";
+                    terminalInput.Text = "";
+                    ExecuteCommand(command, viewModel);
+                }
+
+                if (e.Key == Key.Down)
+                {
+                    viewModel.CmdHistory.IncrementIndex();
+                    terminalInput.Text = viewModel.CmdHistory.GetCommand();
+                }
+                else if (e.Key == Key.Up)
+                {
+                    viewModel.CmdHistory.DecrementIndex();
+                    terminalInput.Text = viewModel.CmdHistory.GetCommand();
+                }
+            }
+        }
+
+        private void ExecuteCommand(string command, MainViewModel viewModel)
+        {
+            switch (command.Split(' ')[0].ToLower())
+            {
+                case "load":
+                    string? filePath = FileService.OpenFileDialog();
+                    if (!string.IsNullOrEmpty(filePath))
+                    {
+                        viewModel.CurrentImagePath = filePath;
+                        viewModel.CurrentImage = FileService.OpenFile(filePath);
+                        terminalOutput.Text += "Loaded " + viewModel.CurrentImagePath;
+                    } else
+                    {
+                        terminalOutput.Text += "No file loaded";
+                    }
+                    break;
+                case "rename":
+                    if (viewModel.CurrentImagePath == null) break;
+                    if (FileService.RenameFile(viewModel.CurrentImagePath, command[7..]))
+                    {
+                        terminalOutput.Text += "Renamed successfully.";
+                    }
+                    break;
+                case "copy":
+                    if (viewModel.CurrentImagePath == null) break;
+                    string destPath = command[5..];
+                    if (FileService.CopyFile(viewModel.CurrentImagePath, destPath))
+                    {
+                        terminalOutput.Text += "Copied successfully.";
+                    }
+                    break;
+                case "exit":
+                    Application.Current.Shutdown();
+                    break;
+
+                case "clear":
+                    terminalOutput.Text = "";
+                    break;
+
+                default:
+                    terminalOutput.Text += "Command not recognized.";
+                    break;
+            }
+            
+            terminalOutput.Text += "\n";
+            viewModel.CmdHistory.AddCommand(command);
+        }
+    }
+}
