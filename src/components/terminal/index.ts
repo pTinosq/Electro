7521b8fb1620e5b4809fbd64d46188502ed7c773
pTinosq <tinosps@gmail.com>
2025-01-18 10:04:12 +0000

    terminal history

--- src/components/terminal/index.ts
+++ src/components/terminal/index.ts
@@ -1,10 +1,56 @@
 import { BaseComponent } from "../baseComponent";
 
 export default class Terminal extends BaseComponent {
+	private inputElement: HTMLInputElement;
+	private historyElement: HTMLElement;
+	private pathElement: HTMLElement;
+
 	constructor(selector: string) {
 		super(selector);
 
-		console.log("Terminal initialized");
+		this.inputElement = this.element.querySelector(
+			"#terminal-input",
+		) as HTMLInputElement;
+		this.historyElement = this.element.querySelector(
+			"#terminal-history",
+		) as HTMLElement;
+		this.pathElement = this.element.querySelector("#path") as HTMLElement;
+
+		if (!this.inputElement || !this.historyElement || !this.pathElement) {
+			throw new Error(
+				"Required terminal elements not found within the component.",
+			);
+		}
+
+		this.addEventListeners();
+	}
+
+	private addEventListeners() {
+		this.element.addEventListener("click", () => {
+			this.inputElement.focus();
+		});
+
+		this.inputElement.addEventListener("keydown", (e: KeyboardEvent) =>
+			this.handleInput(e),
+		);
+	}
+
+	private handleInput(e: KeyboardEvent) {
+		if (e.key === "Enter") {
+			const inputValue = this.inputElement.value.trim();
+
+			this.appendToHistory(inputValue);
+
+			this.inputElement.value = "";
+		}
+	}
+
+	private appendToHistory(text: string) {
+		const newEntry = document.createElement("div");
+
+		// Add a placeholder if the text is empty
+		newEntry.innerHTML = text || "&nbsp;";
+		this.historyElement.appendChild(newEntry);
 	}
 
 	protected updateUI() {
