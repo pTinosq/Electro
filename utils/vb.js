6f64fa9f69b6ae1f6ebd04a7aeeb76324c130569
pTinosq <tinosps@gmail.com>
2025-02-21 01:27:06 +0000

    build(vb): added `npm run vb` command

---
+++ utils/vb.js
@@ -0,0 +1,52 @@
+/*
+VB = Version Bump
+USAGE: `npm run vb`
+
+Script that propagates version changes to the package.json and tauri.conf.json files.
+*/
+
+import { readFileSync, writeFileSync } from "node:fs";
+import { createInterface } from "node:readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askVersion = () => {
+  return new Promise((resolve) => {
+    rl.question("Enter new version (e.g. 0.4.2): ", (version) => {
+      resolve(version.trim());
+    });
+  });
+};
+
+const updateJsonFile = (filePath, updater) => {
+  try {
+    const json = JSON.parse(readFileSync(filePath, "utf8"));
+    updater(json);
+    writeFileSync(filePath, `${JSON.stringify(json, null, 2)}\n`, "utf8");
+    console.log(`Updated ${filePath}`);
+  } catch (error) {
+    console.error(`Error updating ${filePath}:`, error);
+  }
+};
+
+(async () => {
+  const newVersion = await askVersion();
+  rl.close();
+
+  if (!/^\d+\.\d+\.\d+$/.test(newVersion)) {
+    console.error("Invalid version format.");
+    process.exit(1);
+  }
+
+  updateJsonFile("./package.json", (json) => {
+    json.version = newVersion
+  });
+  updateJsonFile("./src-tauri/tauri.conf.json", (json) => {
+    json.version = newVersion
+  });
+
+  console.log(`Version updated to ${newVersion}`);
+})();
